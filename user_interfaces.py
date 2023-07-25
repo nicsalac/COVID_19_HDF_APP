07447c72791d04a0872189f16af4d150924c9e7b
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-25 09:30:56 +0800

    setting up HDF app and its GUI components

--- user_interfaces.py
+++ user_interfaces.py
@@ -3,3 +3,11 @@ import tkinter as tk
 import tkinter.scrolledtext as scrolledtext
 from tkinter import messagebox
 from health_declaration_form import HealthDeclarationForm
+
+# define the HealthDeclarationApp class responsible for managing the GUI
+# create an instance of the HealthDeclarationForm class to handle health declaration data
+# entry  for running the application 
+# create the main GUI window
+# create the menu frame at the top of the GUI
+# create the title label and pack it into the menu frame
+     
