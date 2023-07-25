15a6a70b5f520d043552246f9285fa44aa783323
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-25 08:47:00 +0800

    implement add_contact method

--- health_declaration_form.py
+++ health_declaration_form.py
@@ -8,4 +8,14 @@ class HealthDeclarationForm:
         self.contacts = []
         self.filename = filename
 
+    def add_contact(self, contact_info):
+        # method to add new contact to the contacts list
+        if len(self.contacts) >= 1000
+        #check if the contact list is full (max of 1000 records)
+             raise ValueError("HDF Record Book is full. Cannot add more contacts")
+        # append the new contact info to the contact list
+        self.contacts.append(contact_info)
+
         
+
+
