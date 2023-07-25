9d79afba71ee41e5a1c98ac7228a75d5ff652f5c
nicsalac <nicoletteirishsalac127@gmail.com>
2023-07-25 09:18:38 +0800

    importing the necessary modules from tkinter

---
+++ user_interfaces.py
@@ -0,0 +1,5 @@
+# Import the necessary modules 
+import tkinter as tk
+import tkinter.scrolledtext as scrolledtext
+from tkinter import messagebox
+from health_declaration_form import HealthDeclarationForm
